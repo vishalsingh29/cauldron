b59a96d86ffcbe5ec02c9a3aca86782ddd72ce22
vishalsingh29 <vishalsingh.2911@gmail.com>
2020-12-22 01:00:01 +0530

    added expense model

--- cauldron/activity/activity.go
+++ cauldron/activity/activity.go
@@ -1,1 +1,11 @@
 package activity
+
+import "time"
+
+// Expense dao
+type Expense struct {
+	Date     time.Time `db:"time"`
+	Amount   int32     `db:"amount"`
+	Type     string    `db:"type"`
+	ItemName string    `db:"name"`
+}
